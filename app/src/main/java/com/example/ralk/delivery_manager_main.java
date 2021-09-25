aab7d43506d8657d9982313a86a55b4ea97f3d38
daydreamer44 <72969004+daydreamer44@users.noreply.github.com>
2021-09-25 12:05:46 +0530

    gg kollo

--- app/src/main/java/com/example/ralk/delivery_manager_main.java
+++ app/src/main/java/com/example/ralk/delivery_manager_main.java
@@ -2,6 +2,7 @@ package com.example.ralk;
 
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,16 +11,22 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.ralk.model.Deliverylist;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Objects;
 
 public class delivery_manager_main extends AppCompatActivity {
 
  EditText orderno,dpname,dctime;
- Button button2;
+ Button button2,button3;
  DatabaseReference reff;
  Deliverylist deliverylist;
-
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate((savedInstanceState));
@@ -28,6 +35,7 @@ public class delivery_manager_main extends AppCompatActivity {
         dpname=(EditText) findViewById(R.id.dpname);
         dctime=(EditText) findViewById(R.id.dctime);
         button2=(Button) findViewById(R.id.button2);
+        button3=(Button) findViewById(R.id.button3);
         deliverylist=new Deliverylist();
         reff= FirebaseDatabase.getInstance("https://ralk-ef10e-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference("Deliverylist");
 
@@ -39,6 +47,7 @@ public class delivery_manager_main extends AppCompatActivity {
                 deliverylist.setDpname(dpname.getText().toString());
                 deliverylist.setDctime(dctime.getText().toString());
                 deliverylist.setOrderno(orderNo);
+
                 reff.push().setValue(deliverylist);
                 Toast.makeText(delivery_manager_main.this, "data paka", Toast.LENGTH_SHORT).show();
 
@@ -46,6 +55,17 @@ public class delivery_manager_main extends AppCompatActivity {
         });
 
 
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int orderNo=Integer.parseInt(orderno.getText().toString().trim());
+                deliverylist.setDpname(dpname.getText().toString());
+                deliverylist.setDctime(dctime.getText().toString());
+                deliverylist.setOrderno(orderNo);
+
+
+            }
+        });
 
     }
 
