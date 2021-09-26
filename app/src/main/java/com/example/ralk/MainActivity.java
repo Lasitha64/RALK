e97c2647a8f3c2ab272954de7f7c480ccf059380
Kisaja <87978775+Kisaja@users.noreply.github.com>
2021-09-26 10:35:28 +0530

    need to change main

--- app/src/main/java/com/example/ralk/MainActivity.java
+++ app/src/main/java/com/example/ralk/MainActivity.java
@@ -1,18 +1,18 @@
 package com.example.ralk;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.ralk.cus.activity_customer_login;
 import com.example.ralk.cus.activity_customer_signup;
 
 public class MainActivity extends AppCompatActivity {
 
-     Button logindp,logincus,signup,loginp,loginad;
+     Button logindp,logincus,signup,loginp,loginad,logpm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         logindp = findViewById(R.id.btn_logdp);
         logincus = findViewById(R.id.btn_logcus);
         signup = findViewById(R.id.btn_signup);
-        logindp = findViewById(R.id.btn_logdp);
+        logpm = findViewById(R.id.btn_logpm);
         loginad = findViewById(R.id.btn_logAdmin);
 
         logindp.setOnClickListener(new View.OnClickListener() {
@@ -36,6 +36,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, activity_customer_login.class));
+            }
+        });
+
+        logpm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, activity_product_manager_main_page.class));
             }
         });
 
